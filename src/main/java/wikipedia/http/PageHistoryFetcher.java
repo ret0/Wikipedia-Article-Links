33605d21ff74ba5627a07f5bde7437a2b0d3b6f9
Reto Kleeb <retokl@gmail.com>
2011-06-15 16:54:29 -0400

    history fetcher for more than 2 years

--- src/main/java/wikipedia/http/PageHistoryFetcher.java
+++ src/main/java/wikipedia/http/PageHistoryFetcher.java
@@ -26,7 +26,7 @@ import com.google.common.collect.Lists;
  */
 public final class PageHistoryFetcher {
 
-    public static final int MAX_MONTHS = 18;
+    public static final int MAX_MONTHS = 25;
     public static final DateMidnight MOST_RECENT_DATE = new DateMidnight(2011, 6, 1);
 
     private static final int THREAD_SLEEP_MSEC = 1200;
